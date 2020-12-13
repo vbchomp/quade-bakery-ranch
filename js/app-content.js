b89fa60834453ea912d4315ecfa4aaf7668dcaf8
Heather B <vbchomp@gmail.com>
2020-12-12 23:16:33 -0800

    added chat prompt in js and script tag in html

---
+++ js/app-content.js
@@ -0,0 +1,11 @@
+/* console.log("Hello, world"); // check console log in developer tools on browser
+
+var VariableName;
+
+VariableName = "Hello, World!";
+
+var pokemon = prompt(); */
+
+var promptHelp = "Would you like to chat?";
+var response = prompt(promptHelp);
+console.log(response);
--- js/app.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello, world"); // check console log in developer tools on browser
-
-var VariableName;
-
-VariableName = "Hello, World!";
-
-var pokemon = prompt();
-
