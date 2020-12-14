d50975c0818edf758a37f387df3fbf99804e9134
Heather B <vbchomp@gmail.com>
2020-12-14 12:45:21 -0800

    holy crap i added another 2 dynamic components and only one of them works

--- js/app-content.js
+++ js/app-content.js
@@ -10,12 +10,12 @@ var pokemon = prompt(); */
 var cupcake = "Good Morning, Cupcake!";
 console.log(cupcake);
 
-/* JS #2 */
+
 var promptHelp = "Would you like to chat?";
 var response = prompt(promptHelp);
 console.log(response);
 
-/* JS #3 */
+
 var alertMessage;
 if (response === "Yes") {
     alertMessage = "I'm still learning Web Development. Please hold until I can figure out a response.";
---
+++ js/app_math.js
@@ -0,0 +1,18 @@
+/* console.log("Hello, world"); // check console log in developer tools on browser
+
+
+
+/* Cannot stop getting the NaN error. Not sure why it's not adding the two inputs. */
+var length;
+var width;
+var area = lengthResponse * widthResponse;
+var lengthPrompt = "Enter length";
+var widthPrompt = "Enter width";
+var lengthResponse = prompt(lengthPrompt);
+var widthResponse = prompt(widthPrompt);
+console.log(area);
+
+var alertMessage = "The area is " + area + "!";
+
+alert(alertMessage);
+
