c7a31e3f5b10295d3dba2df684f12675a9d2aec1
Heather B <vbchomp@gmail.com>
2020-12-14 12:54:47 -0800

    fixed app-math.js to get area calculating

--- js/app_math.js
+++ js/app_math.js
@@ -5,11 +5,11 @@
 /* Cannot stop getting the NaN error. Not sure why it's not adding the two inputs. */
 var length;
 var width;
-var area = lengthResponse * widthResponse;
 var lengthPrompt = "Enter length";
 var widthPrompt = "Enter width";
 var lengthResponse = prompt(lengthPrompt);
 var widthResponse = prompt(widthPrompt);
+var area = lengthResponse * widthResponse;
 console.log(area);
 
 var alertMessage = "The area is " + area + "!";
