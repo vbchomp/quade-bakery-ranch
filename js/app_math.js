628d1226053e8c78589eab8f841391ebcebdf315
Heather B <vbchomp@gmail.com>
2020-12-14 20:54:40 -0800

    added notes to mod7

--- js/app_math.js
+++ js/app_math.js
@@ -1,8 +1,3 @@
-/* console.log("Hello, world"); // check console log in developer tools on browser
-
-
-
-/* Cannot stop getting the NaN error. Not sure why it's not adding the two inputs. */
 var length;
 var width;
 var lengthPrompt = "Enter length";
