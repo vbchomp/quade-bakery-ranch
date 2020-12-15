5ded804b361a4efc8f72a1dd42ee44770d4e8bcb
Heather B <vbchomp@gmail.com>
2020-12-14 21:33:23 -0800

    fixing js functions

--- js/app-content.js
+++ js/app-content.js
@@ -1,25 +1,41 @@
-/* console.log("Hello, world"); // check console log in developer tools on browser
-
-var VariableName;
-
-VariableName = "Hello, World!";
-
-var pokemon = prompt(); */
-
 /* JS #1 */
 var cupcake = "Good Morning, Cupcake!";
 console.log(cupcake);
 
-
-var promptHelp = "Would you like to chat?";
-var response = prompt(promptHelp);
-console.log(response);
-
-
-var alertMessage;
-if (response === "Yes") {
-    alertMessage = "I'm still learning Web Development. Please hold until I can figure out a response.";
-} else {
-    alertMessage = "Good thing you are not holding your breath!";
-}
+/* Function that asks if you want to chat*/
+function promptChat() {
+    var promptHelp = "Would you like to chat?";
+    var response = prompt(promptHelp);
+    console.log(response);
+  
+    var alertMessage;
+        if (response === "Yes") {
+        alertMessage = "I'm still learning Web Development. Please hold until I can figure out a response.";
+        } else {
+        alertMessage = "Good thing you are not holding your breath!";
+    }
 alert(alertMessage);
+}
+
+/* Calling Chat function */
+promptChat();
+
+
+/* Function to determine the area */
+function areaFunction(); {
+    var length;
+    var width;
+    var lengthPrompt = "Enter length";
+    var widthPrompt = "Enter width";
+    var lengthResponse = prompt(lengthPrompt);
+    var widthResponse = prompt(widthPrompt);
+    var area = lengthResponse * widthResponse;
+    /* console.log(area); */
+
+    var alertMessage = "The area is " + area + "!";
+
+alert(alertMessage);
+}
+
+/* Calling area function */
+areaFunction();
--- js/app_math.js
+++ js/app_math.js
@@ -1,13 +1,1 @@
-var length;
-var width;
-var lengthPrompt = "Enter length";
-var widthPrompt = "Enter width";
-var lengthResponse = prompt(lengthPrompt);
-var widthResponse = prompt(widthPrompt);
-var area = lengthResponse * widthResponse;
-console.log(area);
-
-var alertMessage = "The area is " + area + "!";
-
-alert(alertMessage);
 
